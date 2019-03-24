d568487ddb0aa12f82b01be8a6885226817dc18d
Andrey <crashc@yandex.ru>
2019-03-24 14:21:22 +0500

    Create tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <list>
+#include <cassert>
+
+void test_0 ()
+{
+  typedef andre::stack <10, int> stack_t;
+  stack_t st;
+  assert(st.isEmpty());
+  assert(st.size() == 0);
+}
+
+void test_1 ()
+{
+  typedef andre::stack <10, int> stack_t;
+  stack_t st;
+  
+  for(int i = 0; i < stack_t::max_size; i++)
+    st.Push(i);
+    
+  assert(!st.Empty());
+  assert(st.isFull());
+  assert(st.size() == st.max_size());
+}
+
+void test_2 ()
+{
+  typedef andre::stack <10, int> stack_t;
+  stack_t st;
+  
+  for(int i = 0; i < stack_t::max_size; i++)
+    st.Push(i);
+  assert(st.isFull());
+  
+  for(int i = 0; i < stack_t::max_size; i++)
+    st.Pop();
+  
+  assert(st.size() == 0);
+  
+}
